a5ef065e37d297a02581a58e7928adb610e674ef
Joao Paulo <jp.jpr.jp@gmail.com>
2022-06-27 19:25:05 -0300

    Ajustes finais

--- src/application/Program.java
+++ src/application/Program.java
@@ -42,11 +42,15 @@ public class Program {
 				}
 				
 				if(chessMatch.getPromoted() != null) {
-					System.out.println("Enter piece for promotion (B/N/R/Q): ");
+					System.out.print("Enter piece for promotion (B/N/R/Q): ");
 					String type = sc.nextLine();
+					while(!type.equals("B")&& !type.equals("N")&& !type.equals("R")&& !type.equals("Q") ) {
+					System.out.print("Invalid Value! Enter piece for promotion (B/N/R/Q): ");
+					type = sc.nextLine().toUpperCase();
+					}
 					chessMatch.replacePromotedPiece(type);
-				}
 				
+				}
 			}
 			catch(ChessException e) {
 				System.out.println(e.getMessage());
